1da36f0ad717143ab28a750a7bbee2d2093391ad
DarkRichi <kimchetra83@gmail.com>
2024-09-22 15:43:17 +0700

    Add files via upload

--- Project School Systme/src/Usercontrol.java
+++ Project School Systme/src/Usercontrol.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Admincontrol {
+public class Usercontrol {
     JFrame frame = new JFrame();
-    JLabel text = new JLabel("Hello Admin");
+    JLabel text = new JLabel("Welcome User");
 
-    Admincontrol(){
+    Usercontrol(){
         text.setBounds(0,0,200,50);
         text.setFont(new Font(null, Font.LAYOUT_NO_LIMIT_CONTEXT,20));
         frame.add(text);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Admin");
+        frame.setTitle("User");
         frame.setSize(500,500);
         frame.setLayout(null);
         frame.setVisible(true);
